ba06ca06286c598add046179bfd2741be09d2f50
Fernando-HD <123045484@upq.edu.mx>
2026-03-27 03:42:04 -0600

    fix: remove app import conflict and init db properly

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
-from app import app
+from flask import Flask
 from .contacts import contacts
+from .db import init_db
+
+app = Flask(__name__)
+
+init_db(app)
 
 app.register_blueprint(contacts)
 
-# starting the app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8081, debug=True)
+    app.run(host="0.0.0.0", port=8081)
